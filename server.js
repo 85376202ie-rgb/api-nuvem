b4ae1b29db7fa932c1cfe4135148191810a94f99
85376202ie-rgb <85376202ie@gmail.com>
2026-03-13 09:17:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/" , (req, res ) => {
     res.json({
         mensagem: "API funcionando na NUVEM 🚀" ,
-        status: online 
+        status: "online"
 
     });
 });
@@ -12,3 +12,4 @@ app.get("/" , (req, res ) => {
 app.listen(PORT, () =>{
     console.log(`Servidor rodando na porta ${PORT}`)
 });
+
